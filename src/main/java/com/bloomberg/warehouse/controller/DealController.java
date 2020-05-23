f5130fc4a9fc3f22084911cc9721673c7dc2c2c1
Gewil Ira <gewilira.abiera@gmail.com>
2020-05-23 21:03:12 +0400

    exception handler

--- src/main/java/com/bloomberg/warehouse/controller/DealController.java
+++ src/main/java/com/bloomberg/warehouse/controller/DealController.java
@@ -3,11 +3,16 @@ package com.bloomberg.warehouse.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.bloomberg.warehouse.exceptions.FileAlreadyUploadedException;
+import com.bloomberg.warehouse.exceptions.FileCannotBeParseException;
+import com.bloomberg.warehouse.exceptions.FileNotFoundException;
 import com.bloomberg.warehouse.service.DealService;
 import com.bloomberg.warehouse.service.dto.DealFileResponse;
 
@@ -33,6 +38,15 @@ public class DealController {
 		model.addAttribute("message", fileResponse.getMessage());
 
 		return "upload-status";
+	}
+	
+	@ExceptionHandler({FileAlreadyUploadedException.class, FileCannotBeParseException.class, FileNotFoundException.class})
+	public ModelAndView handleException(RuntimeException ex) {
+		// Do something additional if required
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("upload-status");
+		modelAndView.addObject("message", ex.getMessage());
+		return modelAndView;
 	}
 
 }
---
+++ src/main/java/com/bloomberg/warehouse/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.bloomberg.warehouse.exceptions;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Configuration
+public class GlobalExceptionHandler {
+
+	
+
+}
